ffd547d645c5333ec5a97b81545722aad73ea145
padsley <padsley@gmail.com>
2021-01-18 11:08:49 +0200

    pushing working version to github

--- ProcessMergedData.C
+++ ProcessMergedData.C
@@ -31,6 +31,14 @@ void ProcessMergedData::Loop()
 //by  b_branchname->GetEntry(ientry); //read only this branch
    if (fChain == 0) return;
 
+   TFile *fout = new TFile("ProcessedOutput.root","RECREATE");
+   
+   //PA - Make histograms here (or can make a TTree to store the output if desired
+   TH2F *hPosEgamma = new TH2F("hPosEgamma","Position vs E_{#gamma}",800,0,800,10000,0,100000);
+   hPosEgamma->SetStats(0);
+   TH2F *hPosEgamma_TimeGate = new TH2F("hPosEgamma_TimeGate","Position vs E_{#gamma}",800,0,800,10000,0,100000);
+   hPosEgamma_TimeGate->SetStats(0);
+   
    Long64_t nentries = fChain->GetEntriesFast();
 
    Long64_t nbytes = 0, nb = 0;
@@ -38,6 +46,22 @@ void ProcessMergedData::Loop()
       Long64_t ientry = LoadTree(jentry);
       if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);   nbytes += nb;
+      
+      if(jentry % 100000 ==0)printf("%f %% complete\n",100*(double)jentry/(double)nentries);
+      
       // if (Cut(ientry) < 0) continue;
+      for(int n=0;n<48;n++)
+      {
+        hPosEgamma->Fill(X1pos,Ge_calEnergy[n]);
+        if(Ge_time[n]>0 && (DigTrigTime - Ge_time[n]) > -0.8 && (DigTrigTime - Ge_time[n]) < -0.2)hPosEgamma_TimeGate->Fill(X1pos,Ge_calEnergy[n]);
+      }
+      
    }
+   
+
+   TCanvas *c1 = new TCanvas("c1","c1",800,800);
+   hPosEgamma->Draw("col");
+   
+   TCanvas *c2 = new TCanvas("c2","c2",800,800);
+   hPosEgamma_TimeGate->Draw("col");
 }
